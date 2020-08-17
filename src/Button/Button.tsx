c2944ab4fdd24576aea5ec741a3d25dd9a7d40c2
Ben Glynn <ben@benglynn.com>
2020-08-17 21:55:59 +0100

    button styling

--- src/Button/Button.tsx
+++ src/Button/Button.tsx
@@ -4,18 +4,32 @@ import styled from "styled-components";
 interface Props {
     disabled: boolean;
     label: string;
+    onClick: (fn: (e: Event) => {}) => void;
 }
 
 const StyledButton = styled.button`
+    align-items: center;
+    background: #5d5aa7;
     border-radius: .2em;
     border: 1px solid #333;
+    color: white;
     display: inline-flex;
-    justify-items: center;
+    font-size: 1em;
+    font-weight: 600;
+    justify-content: space-around;
     min-height: 2em;
     min-width: 5em;
     text-align: center;
+    &:disabled {
+        background: #ccc;
+        color: #666;
+    }
 `;
 
-export const Button: React.FC<Props> = ({ disabled, label }) => (
-    <StyledButton className=".Button" disabled={disabled}>{label}</StyledButton>
+export const Button: React.FC<Props> = ({ disabled, label, onClick }) => (
+    <StyledButton
+        className=".Button"
+        disabled={disabled}
+        onClick={onClick}
+        >{label}</StyledButton>
 );
